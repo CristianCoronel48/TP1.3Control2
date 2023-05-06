57b335f1cac4a6587d55c7d68a062e8f6bc5657c
Julián Pucheta <82587926+Julianpucheta@users.noreply.github.com>
2023-05-06 17:41:58 -0300

    Update tp1_3_rlc.m
    Te hice unos cambios, corrigiendo el tiempo de inicio del escalón de 12V de entrada y su correspondiente respuesta en y.
    Además, el valor de K estaba incorrecto.

--- tp1_3_rlc.m
+++ tp1_3_rlc.m
@@ -1,39 +1,27 @@
 % El codigo a partir de leer la repuesta al escalon te devuelve la FT
 %Codigo realizado por JAP 
-% syms k1 k2 k3 alfa1 alfa2 beta
-% s=solve('alfa1*alfa2=(k2^2-k1*k3)/(k1^2+k2)',...
-% 'alfa1+alfa2=(k3+k1*k2)/(k1^2+k2)','alfa1','alfa2');
-% simplify(s.alfa1)
-% simplify(s.alfa2)
-% alfa1=s.alfa1(1);
-% alfa2=s.alfa1(2);
-% s1=solve('beta*(alfa1-alfa2)=k1+alfa2',...
-% 'beta*(alfa1^2-alfa2^2)=k2+alfa2^2',...
-% 'beta*(alfa1^3-alfa2^3)=k3+alfa2^3','beta');
-% sys_G=tf(2*[8 1],conv([5 1],[6 1])); %la otra planta
-%sys_G=tf(16*[45 1],conv([25 1],[30 1]))
-%sys_G=tf(3*[10 1],conv([6 1],[6 1])); %esta tiene los polos iguales
-% sys_G=tf(2*[8 1],([ 1 2 2])); %la otra planta
-% opt = stepDataOptions;
-% opt.StepAmplitude = 1;
-% t_s=0:2:50;
+% El codigo a partir de leer la repuesta al escalon te devuelve la FT
+%Codigo realizado por JAP
+%Revisar en https://github.com/Julianpucheta/OptimalControl/blob/main/ID_CHEN.ipynb
 clear all; clc;
 [x1]=xlsread('Curvas_Medidas_RLC'); % lee los datos de la planilla excel
 t0=x1(:,1);%vector de tiempo
-y=x1(:,3);
-t_inic= .02; %tiempo de inicio de la simulacion
-% 
-[val,lugar] = min(abs(t_inic-t0)); y_t1=y(lugar);
-t_t1= t0(lugar);
-ii=0; % for t_inic=10:15
+y=x1(:,3); figure;plot(t0,y,'.')
+t_ret= .0101; %Tiempo de offset, en el que empieza a existir entrada escalón
+t_inic= .0131 - t_ret; %tiempo al que se debe restar el tiempo arranque de escalón
+[val,lugar] = min(abs(t_inic+t_ret-t0)); y_t1=y(lugar);
+t_t1= t0(lugar) - t_ret; %Escala t1 de Chen
+ii=0;
 ii=ii+1;
-[val,lugar] =min(abs(2*t_inic-t0));
-t_2t1= t0(lugar);
+[val,lugar] =min(abs(2*t_inic + t_ret -t0));
+t_2t1= t0(lugar) - t_ret;
 y_2t1=y(lugar);
-[val,lugar] =min(abs(3*t_inic-t0));
-t_3t1= t0(lugar);
+[val,lugar] =min(abs(3*t_inic + t_ret-t0));
+t_3t1= t0(lugar) - t_ret;
 y_3t1=y(lugar);
-K=y(end)/12; %valor del escalon de 12 voltios
+t_end=.0483;
+[val,lugar] =min(abs(t_end-t0));
+K=y(lugar)/12; %valor del escalon de 12 voltios
 k1=(1/12)*y_t1/K-1;%Afecto el valor del Escalon
 k2=(1/12)*y_2t1/K-1;
 k3=(1/12)*y_3t1/K-1;
@@ -50,11 +38,10 @@ T3(ii)=T3_ang;
 T3_ang=sum(T3/length(T3));
 T2_ang=sum(T2/length(T2));
 T1_ang=sum(T1/length(T1));
-sys_G_ang=tf(K,conv([T1_ang 1],[T2_ang 1]))
-% [yo,to]=step(12*sys_G_ang,.1-t_off);
-step(12*sys_G_ang);
+sys_G_ang=tf(K,conv([real(T1_ang) 1],[real(T2_ang) 1]))
+[yo,to]=step(12*sys_G_ang); % step(12*sys_G_ang);
 plot(t0,y,'r'), hold on;
-plot(t_t1+0.02019,y_t1,'o')
-plot(t_2t1+0.02019,y_2t1,'o')
-plot(t_3t1+0.02019,y_3t1,'o')
-% plot(to+t_off,yo)
+plot(t_t1 + t_ret,y_t1,'o')
+plot(t_2t1+ t_ret,y_2t1,'o')
+plot(t_3t1+ t_ret,y_3t1,'o')
+plot(to+t_ret,yo,'.k')
